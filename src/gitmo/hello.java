b11f361c45fcbfbc581b360a32855a49dca15f88
mithar <castrodennis97@gmail.com>
2017-07-19 22:17:36 -0400

    henlo

---
+++ src/gitmo/hello.java
@@ -0,0 +1,11 @@
+package gitmo;
+
+public class hello {
+	public static void main(String [] args)
+	{
+		System.out.println("sup m80s");
+		System.out.print("sssss");
+		System.out.println("Hello again");
+	}
+
+}
